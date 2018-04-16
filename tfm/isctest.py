737d208b7088cbf556b7cd717bd63859bce880fd
Daniel Gomez <d.gomez@donders.ru.nl>
2018-04-16 15:57:10 +0200

    Checkpoint isctest

--- tfm/isctest.py
+++ tfm/isctest.py
@@ -17,8 +17,8 @@ clustering=ISCTEST(spatialPattTens,alphaFP,alphaFD,'mixing') tests
 
 See http://www.cs.helsinki.fi/u/ahyvarin/code/isctest/ for more information
 """
-import re
 import os
+import glob
 
 import matlab
 import matlab.engine
@@ -27,8 +27,23 @@ import nibabel as nib
 import matplotlib.pyplot as plt
 
 
+PATTERN = '/project/3015046.07/derivatives/resting-state/fmriprep/sub-0*/ses-0*/func/sub-0*_ses-0*_task-rest_run-*_bold_space-MNI152NLin2009cAsym_preproc_highpass100.ica/tfm.ica/melodic_IC.nii.gz'
+
+
+def generate_patterns(images):
+    """This function generates a ISCTEST compatible spatial
+    pattern, for use with the isctest function.
+
+    images is a glob template
+    """
+    files = glob.glob(images)
+    niftidata = [nib.load(f).get_data() for f in files]
+    patterns = [np.reshape(d, (-1, d.shape[-1])) for d in niftidata]
+    return np.stack(patterns, axis=-1)
+
+
 def isctest(spatial_patterns, *, fpr=0.005, fdr=0.05,
-            test='components', engine=None):
+            test='components', engine=None, async=False):
     """
     spatial_patterns: 3-D tensor which gives the vectors to be tested. Each
      matrix spatialPattTens(:,:,k) is the matrix of the spatial patterns for
@@ -45,7 +60,7 @@ def isctest(spatial_patterns, *, fpr=0.005, fdr=0.05,
         raise NameError('Only \'mixing\' and \'components\' are acceptable tests.')
 
     data = matlab.double(spatial_patterns.tolist())
-    res = engine.isctest(data, fpr, fdr, test)
+    res = engine.isctest(data, fpr, fdr, test, async=async)
     # Convert from matlab back to python before returning
     return np.array(list(map(list, list(res)))).astype(int) - 1
 
@@ -66,11 +81,12 @@ def visualize_all_clusters(data, res):
         visualize_cluster(data, res, index, ax=ax)
 
 
-def save_cluster_components(filename):
+def save_cluster_components(res, filename):
     def patch_filename(inname):
         return os.path.join(os.path.dirname(inname),
                             'melodic_IC.nii.gz')
-
+    # Data here is the list of melodic_mix filenames, i.e.,
+    # the "image" argument passed to generate_patterns.
     arrs = [(nib.load(patch_filename(f)), c)
             for f, c in zip(data, res[0]) if c >= 0]
     affine = arrs[0][0].affine
